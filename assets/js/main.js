f64187c68bd0dcca0e5174395911b2d39eb4a933
mamunhshimul <mamunhshimul@gmail.com>
2020-05-01 23:30:43 +0600

    added show and hide functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,36 @@
 
 filterSelection("all") //  show all item for defult 
 function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("gallery-item");
+  var g_item, i;
+  g_item = document.querySelector(".gallery-item");
   if (c == "all") c = "";
 
    for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    g_RemoveClass(g_item[i], "show");
+    if (g_item[i].className.indexOf(c) > -1) g_AddClass(g_item[i], "show");
   }
 }
 
 
+
+
+function g_AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function g_RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
